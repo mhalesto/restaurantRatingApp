559adcf4d280e1c432b7e747e0554d9debeb87b5
Halalisani <halalisani@iamsignific.com>
2021-12-08 17:02:20 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Restaurant Reviews',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
