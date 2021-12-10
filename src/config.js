95bb070be2f9b3f878ac520ca75af30e0d31c44f
mhalesto <mbanjwa.hg@gmail.com>
2021-12-11 01:36:22 +0200

    fix config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:4000/api/v1';
-// export const API_URL = 'http://ec2-18-219-104-169.us-east-2.compute.amazonaws.com:4000/api/v1';
+// export const API_URL = 'http://localhost:4000/api/v1';
+export const API_URL = 'http://ec2-18-219-104-169.us-east-2.compute.amazonaws.com:4000/api/v1';
 
 // password: restaurants1
 // port: 5432
